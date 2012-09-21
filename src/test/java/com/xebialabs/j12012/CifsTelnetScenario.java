73c4fe26f8bc79fc131db085f6c761658b79f725
Vincent Partington <vpartington@xebialabs.com>
2012-09-21 10:06:55 +0200

    Added scenarios for SSH/SFTP, CIFS/Telnet and CIFS/WinRM.
    - The CIFS/WinRM scenario requires a version of Overthere that supports startProcess in WinRM.

---
+++ src/test/java/com/xebialabs/j12012/CifsTelnetScenario.java
@@ -0,0 +1,46 @@
+package com.xebialabs.j12012;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.util.Collections;
+import java.util.Map;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.xebialabs.overcast.CloudHost;
+import com.xebialabs.overcast.CloudHostFactory;
+
+public class CifsTelnetScenario extends ScenarioTest {
+    private CloudHost host;
+
+    @BeforeClass
+    public void setupHost() {
+        host = CloudHostFactory.getCloudHost("glassfish_cifs");
+        host.setup();
+    }
+
+    @AfterClass
+    public void teardownHost() {
+        host.teardown();
+    }
+
+    @Override
+    protected void initTarget() throws IOException {
+        final Map<String, String> map = Collections.singletonMap("password", "xeb1aLabs");
+        targetFileSystem = FileSystems.newFileSystem(URI.create("cifs://Administrator@" + host.getHostName() + "/?os=WINDOWS&connectionType=TELNET"), map);
+        targetCopyPathName = "C:\\temp";
+        unzipPathName = "C:\\temp\\unzip.exe";
+    }
+
+    @Override
+    protected String getIp() {
+        return host.getHostName();
+    }
+
+    @Test
+    public void neededForTestNGPluginForEclipseToRecognizeThisClassAsATest() { }
+
+}
---
+++ src/test/java/com/xebialabs/j12012/SshSftpScenario.java
@@ -0,0 +1,46 @@
+package com.xebialabs.j12012;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.util.Collections;
+import java.util.Map;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.xebialabs.overcast.CloudHost;
+import com.xebialabs.overcast.CloudHostFactory;
+
+public class SshSftpScenario extends ScenarioTest {
+    private CloudHost glassfish_ssh;
+
+    @BeforeClass
+    public void setupHost() {
+        glassfish_ssh = CloudHostFactory.getCloudHost("glassfish_ssh");
+        glassfish_ssh.setup();
+    }
+
+    @AfterClass
+    public void teardownHost() {
+        glassfish_ssh.teardown();
+    }
+
+    @Override
+    protected void initTarget() throws IOException {
+        final Map<String, String> map = Collections.singletonMap("password", "overhere");
+        targetFileSystem = FileSystems.newFileSystem(URI.create("ssh+sftp://overthere@" + glassfish_ssh.getHostName() + "/?os=UNIX"), map);
+        targetCopyPathName = "/tmp";
+        unzipPathName = "unzip";
+    }
+
+    @Override
+    protected String getIp() {
+        return glassfish_ssh.getHostName();
+    }
+
+    @Test
+    public void neededForTestNGPluginForEclipseToRecognizeThisClassAsATest() { }
+
+}
