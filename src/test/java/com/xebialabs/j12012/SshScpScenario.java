54bb83a0ffc331eed214a6b64aa825895fc21689
Vincent Partington <vpartington@xebialabs.com>
2012-09-21 10:06:00 +0200

    Renamed "overthere_ssh" cloud host to "glassfish_ssh".

--- src/test/java/com/xebialabs/j12012/SshScpScenario.java
+++ src/test/java/com/xebialabs/j12012/SshScpScenario.java
@@ -2,41 +2,45 @@ package com.xebialabs.j12012;
 
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.util.Collections;
 import java.util.Map;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import com.google.common.collect.Maps;
+import org.testng.annotations.Test;
 
 import com.xebialabs.overcast.CloudHost;
 import com.xebialabs.overcast.CloudHostFactory;
 
 public class SshScpScenario extends ScenarioTest {
-    private CloudHost overthere_ssh;
+    private CloudHost glassfish_ssh;
 
     @BeforeClass
     public void setupHost() {
-        overthere_ssh = CloudHostFactory.getCloudHost("overthere_ssh");
-        overthere_ssh.setup();
+        glassfish_ssh = CloudHostFactory.getCloudHost("glassfish_ssh");
+        glassfish_ssh.setup();
     }
 
     @AfterClass
     public void teardownHost() {
-        overthere_ssh.teardown();
+        glassfish_ssh.teardown();
     }
 
     @Override
     protected void initTarget() throws IOException {
         final Map<String, String> map = Collections.singletonMap("password", "overhere");
-        targetFileSystem = FileSystems.newFileSystem(URI.create("ssh+scp://overthere@" + overthere_ssh.getHostName() + "/?os=UNIX"), map);
+        targetFileSystem = FileSystems.newFileSystem(URI.create("ssh+scp://overthere@" + glassfish_ssh.getHostName() + "/?os=UNIX"), map);
         targetCopyPathName = "/tmp";
         unzipPathName = "unzip";
     }
 
     @Override
     protected String getIp() {
-        return overthere_ssh.getHostName();
+        return glassfish_ssh.getHostName();
     }
+
+    @Test
+    public void neededForTestNGPluginForEclipseToRecognizeThisClassAsATest() { }
+
 }
